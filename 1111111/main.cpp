38a24ed482262ac738cb7257b63ce1c37d95d329
VOID001 <zhangjianqiu_133@yeah.net>
2014-02-26 16:13:37 +0800

    大神写的好用的UVA123 借鉴借鉴

--- 1111111/main.cpp
+++ 1111111/main.cpp
@@ -1,8 +1,30 @@
 #include<stdio.h>
-int main(void)
+int a[500002],b[500002];
+int main()
 {
-    int a,b;
-    scanf("%d%d",&a,&b);
-    printf("the result of %d+%d is %d",a,b,a+b);
+    int n,i,j,lie;
+    while(scanf("%d",&n)!=EOF)
+    {
+        lie=0;
+        for(i=1;i<=n;i++)
+            scanf("%d",&a[i]);
+        for(i=1;i<=n;i++)
+            scanf("%d",&b[i]);
+        //if(n==1) {printf("1\n"); continue;}
+        for(i=1;i<=n;i++)
+        {
+            if(b[i]-i==1||b[i]-i==-1) continue;
+            if(b[i]==i) lie++;
+            if(b[i]>i)
+            {
+                for(j=i+1;j<b[i];j++) {if(a[j]>a[i]) lie++; break;}
+            }
+            if(b[i]<i)
+            {
+                for(j=i-1;j>b[i];j--) {if(a[j]>a[i]) lie++; break;}
+            }
+        }
+        printf("%d\n",lie);
+    }
     return 0;
 }
--- 123/123/123.cpp
+++ 123/123/123.cpp
@@ -1,36 +1,43 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#define MAX 10000
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <map>
+#include <set>
+#include <cctype>
+#include <algorithm>
+using namespace std;
+const int maxn = 220;
 
-int cmp(const void *a, const void *b);
-char iglst[55][30];
-char titles[200][MAX];
-
-int main(void)
-{
-	int oo = 0;
-	while (gets(iglst[oo]))
-	{
-		if (!strcmp(iglst[oo], "::")) break;
-		else oo++;
-	}
-	int cnt = 0;
-	while (gets(titles[cnt]) != NULL)
-	{
-		char tmp[10000];
-		strcpy(tmp, titles[cnt]);
-		for (int i = 0; i <= oo; i++)
-		{
-			if (strstr(tmp, iglst[oo]) != NULL)
-			{
-				char *pt = strstr(tmp, iglst[oo]);
-				while (*pt != ' ')
-				{
-					*pt = " ";
-					pt++;
-				}
+int main() {
+	string k, st;
+	int a = 0;
+	set<string> o;
+	multimap<string, string> r;
+	while (cin >> k && k != "::")
+		o.insert(k);
+	getchar();
+	while (getline(cin, st)) {
+		for (int i = 0; i < st.size(); i++)
+			st[i] = tolower(st[i]);
+		for (int i = 0; i < st.size(); i++) {
+			if (!isalpha(st[i])) continue;
+			string t;
+			int rec = i;
+			while (i < st.size() && isalpha(st[i]))
+				t += st[i++];
+			//cout << t << " " << endl;;
+			if (!o.count(t)) {								//如果这个集合o里 有t那么就不执行 没有的话说明是关键词 执行
+				cout << t << " " << endl;
+				for (int j = 0; j < t.size(); j++)
+					t[j] = toupper(t[j]);
+				string temp = st;							//产生标题字串副本
+				temp.replace(rec, t.size(), t);				//用replace把标题字串temp中 与关键子字串位置相同的字符替换为 t 
+				r.insert(make_pair(t, temp));
 			}
 		}
 	}
+	for (multimap<string, string>::iterator i = r.begin(); i != r.end(); i++)		//multimap本身就是排好序的 但是依靠什么顺序还要研究
+		cout << i->second << endl;
+	getchar();
+	return 0;
 }
